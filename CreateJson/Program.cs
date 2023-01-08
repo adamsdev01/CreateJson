2e6cba4f429d5e31f1079f373251a024af3d07b8
Tiffany Adams <tadams03@mail.roosevelt.edu>
2023-01-07 21:57:52 -0600

    Console app for creating json objects

---
+++ CreateJson/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using CreateJson;
+using Newtonsoft.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Record record = new Record();
+
+        string JSONresult = JsonConvert.SerializeObject(record);
+
+        // Create a path locally - Named a folder "json" in my C Drive
+
+        // String Path - add the name of the file to be created 
+        string path = @"C:\json\records.json";
+
+        // If the path location contains a file
+        // with the same name - delete the first one 
+        // and create a new one!
+
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            using (var stream = new StreamWriter(path, true))
+            {
+                stream.WriteLine(JSONresult.ToString());
+                stream.Close();
+            }
+        }
+        else if (!File.Exists(path))
+        {
+            using (var stream = new StreamWriter(path, true))
+            {
+                stream.WriteLine(JSONresult.ToString());
+                stream.Close();
+            }
+        }
+
+    }
+}
---
+++ CreateJson/Record.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJson
+{
+    public class Record
+    {
+        public int RecordId { get; set; } = 1;
+        public string RecordName { get; set; } = "Astroworld";
+        public string ArtistName { get; set; } = "Travis Scott";
+        public DateTime? PurchaseDate { get; set; } = null;
+
+    }
+}
